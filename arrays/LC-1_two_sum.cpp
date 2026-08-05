c3e4894c9de646057e3a1f0e88d08be17dd6db5f
Dev Mangal <dev.mangal17@gmail.com>
2026-08-05 19:05:52 +1000

    optimal two sum

--- arrays/LC-1_two_sum.cpp
+++ arrays/LC-1_two_sum.cpp
@@ -4,24 +4,38 @@ using namespace std;
 //given a vector, return the indices of two elements whose sum equal to target
 class Solution {
 public:
-    //basic O(n2) solution
-    vector<int> twoSum(vector<int>& nums, int target) {
-        int num1 = 0;
-        int num2 = 0;
+    //returns the values themselves
+    //can also do using sort and two pointer approach, calculate the sum and if sum > target then reduce the right pointer, if sum < target then increase the left pointer till sum is found and left < right
+    vector<int> twoSum2(vector<int>& nums, int target) {
+        //this map stores the frequency of the value, not the index
+        unordered_map<int, int> mp;
         for(int i = 0; i < nums.size(); i++){
-            for(int j = i + 1; j < nums.size(); j++){
-                if(nums[i] + nums[j] == target){
-                    num1 = i;
-                    num2 = j;
-                }
-            }
+            int need = target - nums[i];
+            //count just returns 1 or 0, doesnt care about the frequency i.e the value mapped to the key
+            if(mp.count(need)) return {nums[i], need};
+            mp[nums[i]]++;
         }
-        return {num1, num2};
+        return {-1,-1};
+    }
+    vector<int> twoSum(vector<int>& nums, int target){
+        unordered_map<int, int> mp;
+        for(int i = 0; i < nums.size(); i++){
+            int need = target - nums[i];
+            if(mp.count(need)) return {i, mp[need]};
+            //do this step after the if condition since if duplicate values like {3,3} then the current element is the second 3 and we need the first 3's index, if this is done before the if then we get the same index twice
+            mp[nums[i]] = i;
+        }
+        return {-1,-1};
     }
 };
 
 int main(){
     Solution sol;
-    
+    vector<int> nums = {2,7,11,15};
+    int target = 18;
+    vector<int> result = sol.twoSum(nums, target);
+    for(auto it : result){
+        cout << it << endl;
+    }
     return 0;
 }
