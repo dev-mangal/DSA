77131f11d2690e3c41ab6eef90ef9579a284acca
Dev Mangal <dev.mangal17@gmail.com>
2026-08-05 13:47:30 +1000

    optimal two pointer

--- arrays/union_sorted_arr.cpp
+++ arrays/union_sorted_arr.cpp
@@ -2,26 +2,52 @@
 using namespace std;
 
 //two sorted arrays of size m and n, print union of the two in increasing order
-//two pointer approach, set, map
+//two pointer approach (O(M+N), O(M+N)), set(O((M+N)log(M+N))), map(O((N+M)log(N+M)))
 class Solution{
 public:
     void union_sorted(vector<int> &arr1, int m, vector<int> &arr2, int n){
-        map<int,int> freq;
-        for(int i = 0; i < m; i++){
-            freq[arr1[i]]++;
+        //{1,2,3,4} {2,3,4,6,9}
+        vector<int> union_arr{};
+        int i = 0;
+        int j = 0;
+        while(i < m && j < n){
+            if(arr1[i] < arr2[j]){
+                //condition to check if the union is empty or given array doesnt contain duplicates only then insert the current one
+                if(union_arr.empty() || union_arr.back() != arr1[i])
+                    union_arr.push_back(arr1[i]);
+                i++; //increment outside the condition since if duplicate found then also it has to incremet without inserting the element otherwise infinite loop
+            }
+            else if(arr2[j] < arr1[i]){
+                if(union_arr.empty() || union_arr.back() != arr2[j])
+                    union_arr.push_back(arr2[j]);
+                j++;
+            }
+            else{
+                if(union_arr.empty() || union_arr.back() != arr1[i])
+                    union_arr.push_back(arr1[i]);
+                i++;
+                j++;
+            }
         }
-        for(int i = 0; i < n; i++){
-            freq[arr2[i]]++;
+        while(i < m){
+            if(union_arr.empty() || union_arr.back() != arr1[i])
+                union_arr.push_back(arr1[i]);
+            i++;
         }
-        for(auto &it : freq){
-            cout << it.first << endl;
+        while(j < n){
+            if(union_arr.empty() || union_arr.back() != arr2[j])
+                union_arr.push_back(arr2[j]);
+            j++;
+        }
+        for(auto it : union_arr){
+            cout << it << endl;
         }
     }
 };
 
 int main(){
     Solution sol;
-    vector<int> arr1 = {1,2,3,4};
+    vector<int> arr1 = {1,2,2,3,4};
     int m = arr1.size();
     vector<int> arr2 = {2,3,4,6,9};
     int n = arr2.size();
