2ffb3b059374023d6a11b256cdd8786975513a87
Dev Mangal <dev.mangal17@gmail.com>
2026-08-08 17:24:03 +1000

    optimal

--- arrays/find_missing_no.cpp
+++ arrays/find_missing_no.cpp
@@ -2,49 +2,24 @@
 using namespace std;
 
 //given array of size n-1, range [1,n], find the missing number from this range
-//approach : can sort array in nlogn, then using a loop can just find the element
+//approach : sum of first n elements - sum of array (O(N), O(1)) might overflow for large integers
 class Solution{
 public:
-    int partition(vector<int> &arr, int low, int high){
-        int pivot = arr[low];
-        int i = low;
-        int j = high;
-        while(i < j){
-            while(i < high && arr[i] <= pivot){
-                i++;
-            }
-            while(j > low && arr[j] > pivot){
-                j--;
-            }   
-            if(i < j) swap(arr[i], arr[j]);
+    int find_missing(vector<int> &arr){
+        int range = arr.size()+1;
+        int req_sum = range*(range+1)/2;
+        int sum = 0;
+        for(auto it : arr){
+            sum += it;
         }
-        swap(arr[j], arr[low]);
-        return j;
-    }
-    void quick_sort(vector<int> &arr, int low, int high){
-        if(low < high){
-            int p_index = partition(arr, low, high);
-            quick_sort(arr, low, p_index-1);
-            quick_sort(arr, p_index+1, high);
-        }
-    }
-    int find_missing(vector<int> &arr, int n){
-        int range = n+1;
-        quick_sort(arr, 0, n-1);
-        for(int i = 1; i < range; i++){
-            if(arr[i - 1] != i){
-                return i;
-            }
-        }
-        if(arr[n-1] != range) return range;
-        return 0;
+        return req_sum - sum;
     }
 };
 
 int main(){
     Solution sol;
-    vector<int> arr = {6,2,4,5,3,1,7};
-    int result = sol.find_missing(arr, arr.size());
+    vector<int> arr = {6,2,4,5,3,7,8};
+    int result = sol.find_missing(arr);
     cout << result << endl;
     return 0;
 }
