786feea8215980f578d8e0f7b68ea700c7ed0f8e
Dev Mangal <dev.mangal17@gmail.com>
2026-08-11 22:57:17 +1000

    optimal (minimum rolling)

--- arrays/medium/LC-121_buy_sell_stock.cpp
+++ arrays/medium/LC-121_buy_sell_stock.cpp
@@ -4,19 +4,14 @@ using namespace std;
 //prices[i] is the price of given stock at ith day, return the maximum profit achievable, if cant then return 0 (buy on a single day, and sell in the future)
 class Solution{
 public:
-    //approach : prefix, basically for every element find the previous minimum element and store the difference, then get max difference
-    //either using nested loop or store {index,value} in map and use if condition
+    //approach : rolling minimum, save the minimum value and keep iterating, store the max diff
     int maxProfit(vector<int> &prices){
         unordered_map<int, int> mp;
         int max_profit = 0;
-        int max_value = INT_MIN;
+        int min_price = INT_MAX;
         for(int i = 0; i < prices.size(); i++){
-            for(int j = i+1; j < prices.size(); j++){
-                if(prices[j] > prices[i]){
-                    max_value = max(prices[j], max_value);
-                    max_profit = max(max_value - prices[i], max_profit);
-                }
-            }
+            min_price = min(prices[i], min_price);
+            max_profit = max(prices[i] - min_price, max_profit);
         }
         return max_profit;
     }
