7148189a517671be96b964f459f7a8862eac500e
Dev Mangal <dev.mangal17@gmail.com>
2026-08-11 17:53:15 +1000

    print subarray using same logic

--- arrays/medium/LC-53_max_subarray.cpp
+++ arrays/medium/LC-53_max_subarray.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 //find the subarray with the maximum sum and return the sum
+//also print the subarray
 class Solution{
 public:
     //approach : using prefix sum
@@ -9,17 +10,32 @@ public:
         int sum = 0;
         int max_sum = INT_MIN;
         int min_prefix = 0;
+        int min_index = -1;
+        int start_index = 0;
+        int end_index = 0;
         for(int i = 0; i < nums.size(); i++){
             sum += nums[i]; //current sum
-            max_sum = max(sum-min_prefix, max_sum);
-            min_prefix = min(min_prefix, sum);  
+            if(sum - min_prefix > max_sum){
+                max_sum = sum - min_prefix;
+                start_index = min_index + 1;
+                end_index = i;
+            }
+            if(sum < min_prefix){
+                min_prefix = sum;
+                min_index = i;
+            }
         }
+        for(int i = start_index; i <= end_index; i++){
+            cout << nums[i] << endl;
+        }
+        cout << endl;
         return max_sum;
     }
 };
 
 int main(){
     Solution sol;
-    
+    vector<int> nums = {-5,-4,-1,-7,-8};
+    cout << sol.maxSubArray(nums) << endl;
     return 0;
 }
